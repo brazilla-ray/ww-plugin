239f210dabc74bd9106eb6888e51daa442377c7c
William Whitaker <billydwhitaker@gmail.com>
2020-08-12 17:12:21 -0400

    registers custom post type(artwork)

---
+++ ww-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: ww-plugin
+ */
+
+function ww_custom_post_artwork() {
+  register_post_type( 'artwork',
+    array(
+      'labels' => array(
+        'name' => __( 'Artwork', 'textdomain' ),
+        'singular_name' => __( 'Artwork', 'textdomain' ),
+      ),
+        'public'      => true,
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'supports' => array( 'title', 'editor', 'custom-fields' ),
+        'rewrite'     => array( 'slug' => 'artwork' ),
+        'description' => ( 'Custom post type for displaying artwork' ),
+    )
+  );
+}
+add_action( 'init', 'ww_custom_post_artwork' );
