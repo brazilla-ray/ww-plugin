ea519f361ac9bd9d4093946aed85c1c749c0f0ae
William Whitaker <billydwhitaker@gmail.com>
2020-08-18 14:14:42 -0400

    adds message to footer showing template file

--- ww-plugin.php
+++ ww-plugin.php
@@ -20,4 +20,9 @@
    }
  }
 
- 
+ // Show which template file is being used
+ add_action( 'wp_footer', 'wwa_show_template_file' );
+ function wwa_show_template_file() {
+   global $template;
+   print_r( $template );
+ }
